a8d88c088e30b64038145d333f5b5f2bf81fdc30
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-12-11 23:03:02 +0100

    wip

--- packages/gui/src/App.js
+++ packages/gui/src/App.js
@@ -1,30 +1,33 @@
 import React, { useReducer, useContext, useEffect } from 'react'
 import { MyRouter as Router } from './components/router/Router'
 import 'semantic-ui-css/semantic.min.css'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./App.css"
+import "./components/semantic_override/Override.css"
 
 const defaultContext = {
   version: '0.0.1'
-}
+};
 const actions = {
   REPLACE: 'REPLACE'
-}
+};
 const reducer = (state, action) => {
-  const fnName = 'reducer'
+  const fnName = 'reducer';
   if (action.type === actions.REPLACE) {
-    let newState = { ...state, ...action.payload }
+    let newState = { ...state, ...action.payload };
     return newState
   } else {
     throw new Error(`${fnName} - Unsupported reducer action`, { state, action })
   }
-}
-export const Context = React.createContext(defaultContext)
+};
+export const Context = React.createContext(defaultContext);
 
 function App () {
-  const fnName = 'App'
-  const [state, dispatch] = useReducer(reducer, defaultContext)
+  const fnName = 'App';
+  const [state, dispatch] = useReducer(reducer, defaultContext);
   useEffect(() => {
     console.debug(`${fnName} - useEffect - state changed`, { state })
-  }, [state])
+  }, [state]);
   return (
     <div className="App">
       <Context.Provider value={state}>
--- packages/gui/src/components/router/Router.js
+++ packages/gui/src/components/router/Router.js
@@ -1,69 +1,97 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {
-  BrowserRouter as Router,
-  Route,
-  Redirect
+    BrowserRouter as Router,
+    Route,
+    Redirect
 } from 'react-router-dom'
-import { Context } from '../../App'
-import { Button, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import {Context} from '../../App'
+import {Button, Menu, Segment, Sidebar} from 'semantic-ui-react'
+import "./Router.css";
 
-export function MyRouter () {
-  return (
-    <Router>
-      <Route exact={true} path="/" render={(props) => <Home {...props}/>}/>
-      <Route exact={true} path="/users" render={(props) => <Users {...props}/>}/>
-    </Router>
-  )
+export function MyRouter() {
+    return (
+        <Router>
+            <Route exact={true} path="/" render={(props) => <Home {...props}/>}/>
+            <Route exact={true} path="/users" render={(props) => <Users {...props}/>}/>
+        </Router>
+    )
 }
 
-export function Home () {
-  const fnName = 'Home'
-  const context = useContext(Context)
-  const [isVisible, setIsVisible] = useState(false)
-  useEffect(() => {
-    console.debug(`${fnName} - useEffect - context changed`, { context })
-  }, [context])
-  return (
-    <div>
-      <Sidebar.Pushable>
-        <Sidebar
-          animation='overlay'
-          onHide={() => setIsVisible(false)}
-          vertical
-          visible={isVisible}
-        >
-          <div className={"menu"}>
-            <Menu.Item>
-              <Button onClick={() => {setIsVisible(!isVisible)}}>Close Menu</Button>
-            </Menu.Item>
-            <Menu.Item>
-              <Button>Home</Button>
-            </Menu.Item>
-            <Menu.Item>
-              <Button>Users</Button>
-            </Menu.Item>
-          </div>
-        </Sidebar>
-        <Sidebar.Pusher dimmed={isVisible}>
-          <Button onClick={() => {setIsVisible(!isVisible)}}>Menu Toggle</Button>
-          <h1>Home</h1>
-          <div>{JSON.stringify(context)}</div>
-        </Sidebar.Pusher>
-      </Sidebar.Pushable>
-    </div>
-  )
+export function Home() {
+    const fnName = 'Home';
+    const context = useContext(Context);
+    const [isVisible, setIsVisible] = useState(false);
+    useEffect(() => {
+        console.debug(`${fnName} - useEffect - context changed`, {context})
+    }, [context]);
+    return (
+        <div className={fnName}>
+            <Navbar isVisible={isVisible} setIsVisible={setIsVisible}/>
+            <Content isVisible={isVisible} setIsVisible={setIsVisible}/>
+        </div>
+    )
 }
 
-export function Users () {
-  const fnName = 'Users'
-  const context = useContext(Context)
-  useEffect(() => {
-    console.debug(`${fnName} - useEffect - context changed`, { context })
-  }, [context])
-  return (
-    <div>
-      <h1>Users</h1>
-      <div>{JSON.stringify(context)}</div>
-    </div>
-  )
+export function Users() {
+    const fnName = 'Users';
+    const context = useContext(Context);
+    useEffect(() => {
+        console.debug(`${fnName} - useEffect - context changed`, {context})
+    }, [context]);
+    return (
+        <div className={fnName}>
+            <h1>Users</h1>
+            <div>{JSON.stringify(context)}</div>
+        </div>
+    )
 }
+
+export function Navbar(props) {
+    const fnName = 'Navbar';
+    const context = useContext(Context);
+    useEffect(() => {
+        console.debug(`${fnName} - useEffect - context changed`, {context})
+    }, [context]);
+    return (
+        <div className={fnName}>
+            <Button basic onClick={() => {
+                props.setIsVisible(!props.isVisible)
+            }}>
+                <i className="fas fa-bars"/>
+            </Button>
+        </div>
+    )
+}
+
+export function Content(props) {
+    const fnName = 'Content';
+    const context = useContext(Context);
+    useEffect(() => {
+        console.debug(`${fnName} - useEffect - context changed`, {context})
+    }, [context]);
+    return (
+        <div className={fnName}>
+            <Sidebar.Pushable>
+                <Sidebar
+                    animation='overlay'
+                    onHide={() => props.setIsVisible(false)}
+                    visible={props.isVisible}
+                >
+                    <div className={"menu"}>
+                        <Menu.Item>
+                            <Button basic >Home</Button>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button basic >Users</Button>
+                        </Menu.Item>
+                    </div>
+                </Sidebar>
+                <Sidebar.Pusher dimmed={props.isVisible}>
+                    <h1>Home</h1>
+                    <div>{JSON.stringify(context)}</div>
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
+        </div>
+    )
+}
+
