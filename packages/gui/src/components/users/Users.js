1653f3cafd464d906fbb08c374a80afd14823d6f
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-12-13 09:36:38 +0100

    add user creation and toast messages

--- packages/gui/src/components/users/Users.js
+++ packages/gui/src/components/users/Users.js
@@ -4,12 +4,19 @@ import './Users.css'
 import { Navbar } from '../navbar/Navbar'
 import { Content } from '../content/Content'
 import { Button, Table, Form, Tab, Checkbox } from 'semantic-ui-react'
+import iziToast from 'izitoast'
+import 'izitoast/dist/css/iziToast.css'
+
 
 export function Users (props) {
   const fnName = 'Users'
   const context = useContext(Context)
   const [isVisible, setIsVisible] = useState(false)
-  const [userResponse, setUserResponse] = useState(null)
+  const [userResponse, setUserResponse] = useState({
+    data: {
+      users: []
+    }
+  })
   useEffect(() => {
     console.debug(`${fnName} - useEffect - context changed`, { context, props })
     fetchUsers()
@@ -52,26 +59,94 @@ export function Users (props) {
       }
     }
     const getDeleteAction = () => {
+      let buttons = []
       if (context.users.selected.length > 0) {
-        return (
+        buttons.push(
           <Button
             color={'red'}
             onClick={async () => {
-              await deleteSelection();
-              fetchUsers();
+              await deleteSelection()
+              fetchUsers()
             }}>
             Delete
           </Button>
         )
       }
+      if (context.users.selected.length > 0) {
+        buttons.push(
+          <Button
+            color={'grey'}
+            onClick={async () => {
+              context.dispatch({
+                type: context.actions.REPLACE,
+                payload: {
+                  users: {
+                    ...context.users,
+                    selected: []
+                  }
+                }
+              })
+            }}>
+            Deselect all
+          </Button>
+        )
+      }
+      if (context.users.selected.length < userResponse.data.users.length) {
+        buttons.push(
+          <Button
+            color={'grey'}
+            onClick={async () => {
+              context.dispatch({
+                type: context.actions.REPLACE,
+                payload: {
+                  users: {
+                    ...context.users,
+                    selected: userResponse.data.users
+                  }
+                }
+              })
+            }}>
+            Select all
+          </Button>
+        )
+      }
+      return (
+        <React.Fragment>
+          {buttons}
+        </React.Fragment>
+      )
     }
     const getCreateAction = () => {
-      function CreateUserForm(props){
+      function CreateUserForm (props) {
+        const context = useContext(Context)
         const [form, setForm] = useState({
-          username: "", firstname:"", lastname:"", email:""
+          username: '', firstname: '', lastname: '', redirectUri: '', email: ''
         })
-        const handleChange = (event, data) => {
-          console.debug(`${fnName} - handleChange`, {event, data})
+        const handleChange = (event) => {
+          event.persist()
+          const name = event.target.name
+          const value = event.target.value
+          console.debug(`${fnName} - handleChange`, { name, value, event })
+          let subForm = {}
+          subForm[name] = value
+          setForm({
+            ...form,
+            ...subForm
+          })
+        }
+        const onSaveUser = async () => {
+          await context.requests.createUser(form).then(() => {
+            iziToast.info({
+              title: 'Users',
+              message: `Created user ${form.username} (${form.email})`,
+            });
+          }).catch((e) => {
+            iziToast.error({
+              title: 'Users',
+              message: `Error creating user ${form.username} (${form.email}): ${e.message}`,
+            });
+          })
+          await fetchUsers()
         }
         return (
           <Form onSubmit={(event, data) => {
@@ -79,24 +154,29 @@ export function Users (props) {
           }}>
             <Form.Field>
               <label>username</label>
-              <input placeholder='username' value={form.username} onChange={handleChange}/>
+              <input placeholder='username' name={'username'} value={form.username} onChange={handleChange}/>
             </Form.Field>
             <Form.Field>
               <label>firstname</label>
-              <input placeholder='firstname' value={form.firstname} onChange={handleChange}/>
+              <input placeholder='firstname' name={'firstname'} value={form.firstname} onChange={handleChange}/>
             </Form.Field>
             <Form.Field>
               <label>lastname</label>
-              <input placeholder='lastname' value={form.lastname} onChange={handleChange}/>
+              <input placeholder='lastname' name={'lastname'} value={form.lastname} onChange={handleChange}/>
+            </Form.Field>
+            <Form.Field>
+              <label>redirectUri</label>
+              <input placeholder='redirectUri' name={'redirectUri'} value={form.redirectUri} onChange={handleChange}/>
             </Form.Field>
             <Form.Field>
               <label>email</label>
-              <input placeholder='email' type={"email"} value={form.email} onChange={handleChange}/>
+              <input placeholder='email' name={'email'} type={'email'} value={form.email} onChange={handleChange}/>
             </Form.Field>
-            <Button color={"green"} type='submit'>Save</Button>
+            <Button color={'green'} type='submit' onClick={onSaveUser}>Save</Button>
           </Form>
         )
       }
+
       return <CreateUserForm {...props}/>
     }
     const panes = [
@@ -104,8 +184,10 @@ export function Users (props) {
         menuItem: 'Delete',
         render: () => (
           <Tab.Pane>
-            <h4>{context.users.selected.length} user(s) selected.</h4>
-            {getDeleteAction()}
+            <div className={'content'}>
+              <h4>{context.users.selected.length} user(s) selected.</h4>
+              {getDeleteAction()}
+            </div>
           </Tab.Pane>
         )
       },
@@ -113,89 +195,88 @@ export function Users (props) {
         menuItem: 'Create',
         render: () => (
           <Tab.Pane>
-            {getCreateAction()}
+            <div className={'content'}>
+              {getCreateAction()}
+            </div>
           </Tab.Pane>
         )
       }
     ]
     return (
       <div>
-        <Tab panes={panes} />
+        <Tab panes={panes} className={'Actions'}/>
       </div>
     )
   }
   const getTable = () => {
-    try {
-      if (userResponse && userResponse.data) {
-        const getTableHeaderCells = (data) => {
-          let headerCells = [
-            (
-              <Table.HeaderCell key={'selection'}>Selection</Table.HeaderCell>
-            )
-          ]
-          data.users.forEach((user, i) => {
-            // assume the user data has always the same scheme
-            if (i === 1) {
-              for (let p in user) {
-                headerCells.push(
-                  <Table.HeaderCell key={p}>{p.toString()}</Table.HeaderCell>
-                )
-              }
+    const getTableHeaderCells = (data) => {
+      let headerCells = []
+      let attributes = [
+        'selection'
+      ]
+      data.users.forEach((user, i) => {
+        // assume the user data has always the same scheme
+        for (let p in user) {
+          if (user.hasOwnProperty(p)) {
+            if (attributes.includes(p) === false) {
+              attributes.push(p)
             }
-          })
-          return headerCells
-        }
-        const getTableRows = (data) => {
-          const getCells = (user) => {
-            const isSelected = context.users.selected.filter((selectedUser) => {return selectedUser.id === user.id}).length > 0;
-            let cells = [
-              (
-                <Table.Cell key={'selection'} className={'selection'}>
-                  <Checkbox
-                    checked={isSelected}
-                    onChange={((event, data1) => {
-                      console.debug(`${fnName} - checkbox triggered`, { user, event, data1 })
-                      updateSelection(user, event, data1)
-                      // TODO
-                    })}
-                  />
-                </Table.Cell>
-              )
-            ]
-            for (let p in user) {
-              cells.push(
-                <Table.Cell key={p}>{user[p].toString()}</Table.Cell>
-              )
-            }
-            return cells
           }
-          let rows = []
-          data.users.forEach((user, index) => {
-            rows.push(
-              <Table.Row key={index}>
-                {getCells(user)}
-              </Table.Row>
-            )
-          })
-          return rows
         }
-        return (
-          <Table celled>
-            <Table.Header>
-              <Table.Row>
-                {getTableHeaderCells(userResponse.data)}
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {getTableRows(userResponse.data)}
-            </Table.Body>
-          </Table>
+      })
+      attributes.forEach((attribute) => {
+        headerCells.push(
+          <Table.HeaderCell key={attribute}>{attribute.toString()}</Table.HeaderCell>
         )
-      }
-    } catch (e) {
-      console.error(`${fnName} - can't get the users`)
-      return <div>{e.message}</div>
+      })
+      return headerCells
     }
+    const getTableRows = (data) => {
+      const getCells = (user) => {
+        const isSelected = context.users.selected.filter((selectedUser) => {return selectedUser.id === user.id}).length > 0
+        let cells = [
+          (
+            <Table.Cell key={'selection'} className={'selection'}>
+              <Checkbox
+                checked={isSelected}
+                onChange={((event, data1) => {
+                  console.debug(`${fnName} - checkbox triggered`, { user, event, data1 })
+                  updateSelection(user, event, data1)
+                  // TODO
+                })}
+              />
+            </Table.Cell>
+          )
+        ]
+        for (let p in user) {
+          cells.push(
+            <Table.Cell key={p}>{user[p].toString()}</Table.Cell>
+          )
+        }
+        return cells
+      }
+      let rows = []
+      data.users.forEach((user, index) => {
+        rows.push(
+          <Table.Row key={index}>
+            {getCells(user)}
+          </Table.Row>
+        )
+      })
+      return rows
+    }
+    return (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            {getTableHeaderCells(userResponse.data)}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {getTableRows(userResponse.data)}
+        </Table.Body>
+      </Table>
+    )
   }
   return (
     <div className={fnName}>
--- packages/proxy/src/run.js
+++ packages/proxy/src/run.js
@@ -8,6 +8,7 @@ const target = process.env.TARGET_HOST;
 let proxy = httpProxy.createProxyServer({ target: target }).listen(port)
 proxy.on('proxyRes', function(proxyRes, req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH, DELETE, HEAD');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 });
 console.log(`The express-gateway-gui will proxy the incoming requests on port ${port} to ${target}`)
