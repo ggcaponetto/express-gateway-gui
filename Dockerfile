ee268d8d2958741862d27d179b184128de091b92
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-12-13 18:40:52 +0100

    wip

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,20 @@ WORKDIR /app
 
 # Bundle app source
 COPY . .
+
 ARG REACT_APP_PROXY_URL
 ENV REACT_APP_PROXY_URL=$REACT_APP_PROXY_URL
-RUN echo "Environment variable REACT_APP_PROXY_URL: " && echo "$REACT_APP_PROXY_URL"
+
+ARG EXPRESS_GATEWAY_ADMIN_URL
+ENV EXPRESS_GATEWAY_ADMIN_URL=$EXPRESS_GATEWAY_ADMIN_URL
+
+ARG PROXY_PORT
+ENV PROXY_PORT=$PROXY_PORT
+
+RUN echo "Environment variable REACT_APP_PROXY_URL: $REACT_APP_PROXY_URL"
+RUN echo "Environment variable EXPRESS_GATEWAY_ADMIN_URL: $EXPRESS_GATEWAY_ADMIN_URL"
+RUN echo "Environment variable PROXY_PORT: $PROXY_PORT"
+
 RUN echo "Current directory is: " && pwd
 RUN npm i
 RUN npx lerna bootstrap
--- packages/proxy/src/run.js
+++ packages/proxy/src/run.js
@@ -3,12 +3,29 @@ var http = require('http'),
 //
 // Create your proxy server and set the target in the options.
 //
-const port = process.env.PROXY_PORT;
-const target = process.env.EXPRESS_GATEWAY_ADMIN_URL;
-let proxy = httpProxy.createProxyServer({ target: target }).listen(port)
-proxy.on('proxyRes', function(proxyRes, req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH, DELETE, HEAD');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-});
-console.log(`The express-gateway-gui will proxy the incoming requests on port ${port} to ${target}`)
+const port = process.env.PROXY_PORT
+const target = process.env.EXPRESS_GATEWAY_ADMIN_URL
+try {
+  if (!port || !target) throw new Error('The express-gateway-gui needs the PROXY_PORT and EXPRESS_GATEWAY_ADMIN_URL env variables.')
+
+  let proxy = httpProxy.createProxyServer({
+    target
+  })
+  proxy.on('proxyReq', function (proxyReq, req, res) {
+    console.debug(`express-gateway-gui: proxyReq`, { port, target })
+  })
+  proxy.on('proxyRes', function (proxyRes, req, res) {
+    console.debug(`express-gateway-gui: proxyRes`, { port, target })
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH, DELETE, HEAD')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+  })
+  proxy.listen(port)
+
+  proxy.on('error', function (e) {
+    console.log(`Proxy error`, e)
+  })
+  console.log(`The express-gateway-gui will proxy the incoming requests on port ${port} to ${target}`)
+} catch (e) {
+  console.log(`The express-gateway-gui could not create the proxy on port ${port} to ${target}`, e)
+}
